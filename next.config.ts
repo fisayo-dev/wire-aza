350879ebfd34afe9b81ce940b9dbe48f2bf1d447
fisayo-dev <olufisayobadina@gmail.com>
2025-10-28 12:57:03 +0100

    chore: edit next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
