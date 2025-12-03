58f7d1ef9240d12ad1f9ecd6b3ad670a9f0f6eea
fisayo-dev <olufisayobadina@gmail.com>
2025-12-03 20:33:22 +0100

    feat: setup toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono} from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Wire Aza",
@@ -29,6 +29,16 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable}  antialiased`}
       >
         {children}
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: "#00A63D",
+              color: "#FEF3F7",
+            },
+          }}
+        />
       </body>
     </html>
   );
